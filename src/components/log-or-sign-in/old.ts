06682babe91414b902a8e9d93079d2e935ab7582
Melissa Schwartz <melissjs@gmail.com>
2017-12-04 15:57:59 -0800

    authentication and user

--- src/components/log-or-sign-in/old.ts
+++ src/components/log-or-sign-in/old.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { Volunteer} from '../../models/volunteer';
-import { User} from '../../models/user';
 import { VolunteerServiceProvider } from '../../providers/volunteer-service/volunteer-service';
 import { RestServiceProvider } from '../../providers/rest-service/rest-service';
 import { PollingStationServiceProvider } from '../../providers/polling-station-service/polling-station-service';
@@ -16,57 +15,46 @@ import * as globals from '../../globals';
 })
 export class LogOrSignInComponent {
   loginForm: FormGroup;
-  newUser: User;
   volunteerHere: Volunteer;
   loggedIn: boolean;
   errorMessage: string;
-  enterUsername: string;
-  enterPassword: string;
+  enterPhoneNumber: string;
+  enterPasscode: string;
   authenticatingVolunteerPhone: string;
   authenticatingVolunteerPasscode: string;
     
   constructor(private navCtrl: NavController, private alertCtrl: AlertController, public fb: FormBuilder, private pollingstationservice: PollingStationServiceProvider, private volSvc: VolunteerServiceProvider, private restSvc: RestServiceProvider, private authSvc: AuthServiceProvider ) {
     this.loggedIn = false;
     this.loginForm = fb.group({  
-      'enterUsername': ['', Validators.compose([Validators.required, Validators.minLength(3)])],
-      'enterPassword': ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+      'enterPhoneNumber': ['', Validators.compose([Validators.required, Validators.pattern(globals.REGEXPHONE)])],
+      'enterPasscode': ['', Validators.compose([Validators.required, Validators.minLength(8)])]
     });
   }
 
-  onDirtyUsername() {
-    if (this.loginForm.value.enterUsername.length < 3) {
-      this.errorMessage = 'ERROR: Username must be at least 3 characters';
+  onDirtyPhoneNumber() {
+    if (!this.loginForm.value.enterPhoneNumber.match(globals.REGEXPHONE)) {
+      this.errorMessage = 'ERROR: Phone Number must be exactly 10 digits';
+      this.enterPhoneNumber = '';
     } else {
-      this.errorMessage = null;
-      this.enterUsername = this.loginForm.value.enterUsername;
+      this.enterPhoneNumber = this.loginForm.value.enterPhoneNumber;
     }
   }
 
-  onDirtyPassword() {
-    if (this.loginForm.value.enterPassword.length < 8) {
+  onDirtyPasscode() {
+    if (this.loginForm.value.enterPasscode.length < 8) {
       this.errorMessage = 'ERROR: Password is less than 8 characters';
     } else {
-      this.errorMessage = null;
-      this.enterPassword = this.loginForm.value.enterPassword;
+      this.enterPasscode = this.loginForm.value.enterPasscode;
     }
   }
 
   // TEST
   onLogin(): void { 
-    console.log("hi from login", this.loginForm.value.enterUsername);
+    console.log("hi from login", this.loginForm.value.enterPhoneNumber);
   }
 
   onRegister(): void {
-    this.newUser = {
-      username: this.loginForm.value.enterUsername,
-      password: this.loginForm.value.enterPassword,
-      volunteerKey: null
-    }
-    this.authSvc.register(this.newUser)
-      .subscribe( 
-        data => console.log(data),
-        error => console.log(error)
-      );
+    console.log("hi from register", this.loginForm.value.enterPhoneNumber);
   }
 
   // onSubmit(value: any): void { 
