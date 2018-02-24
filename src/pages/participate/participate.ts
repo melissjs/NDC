0e64d902b18b5d88323d3ff31f82acf3d8bb3efc
Melissa Schwartz <melissjs@gmail.com>
2018-02-24 12:00:48 -0800

    refactor participate

--- src/pages/participate/participate.ts
+++ src/pages/participate/participate.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
-// import { HomePage} from '../home/home';
 import { RestServiceProvider } from '../../providers/rest-service/rest-service';
-import { /* FormArray, */ FormBuilder, /* FormControl, */ FormGroup, Validators} from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import { CollaborateForm } from '../../models/collaborate-form'; 
+import { UserServiceProvider } from '../../providers/user-service/user-service';
+import { User } from '../../models/user';
+import { findLast } from '@angular/compiler/src/directive_resolver';
 
 @IonicPage()
 @Component({
   selector: 'page-participate',
   templateUrl: 'participate.html',
 })
-export class ParticipatePage {
+
+export class ParticipatePage implements OnInit {
+
 pageTitle: string;
 collaboratorForm: FormGroup;
 resetName: string;
@@ -19,32 +23,36 @@ resetDesiredContribution: string;
 resetRelevantLinks: string;
 resetContact: string;
 collabFormObj: CollaborateForm;
+regExEmail: string;
+regExPhone: string;
+regExAge: string;
+regExLettersOnly: string;
+regExNumbersOnly: string;
+regExZip: string;
+user: User;
+fullName: string;
 
-constructor(private navCtrl: NavController, navParams: NavParams, private alertCtrl: AlertController, public fb: FormBuilder,
-private restSvc: RestServiceProvider) {
+constructor(private navCtrl: NavController, navParams: NavParams, private alertCtrl: AlertController, public fb: FormBuilder, private restSvc: RestServiceProvider, private userSvc: UserServiceProvider) {
+  this.collabFormObj = null;
+  this.pageTitle = "Participate";
+  this.regExEmail = '[A-Za-z0-9._-][A-Za-z0-9._-]*@[A-Za-z0-9._-][A-Za-z0-9._-]*\.[a-zA-Z][a-zA-Z]*';
+  this.regExPhone = '[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]';
+  this.regExAge= '[1]*[0-9]?[0-9]';
+  this.regExLettersOnly= '[a-ZA-Z]+';
+  this.regExNumbersOnly = '[0-9]*';
+  this.regExZip = '[0-9]{5}[-]?[0-9]?[0-9]?[0-9]?[0-9]?';
+}
 
-this.navCtrl = navCtrl;
-this.collabFormObj = null;
-this.pageTitle = "Participate";
-    //form stuff
-    var regExEmail: string = '[A-Za-z0-9._-][A-Za-z0-9._-]*@[A-Za-z0-9._-][A-Za-z0-9._-]*\.[a-zA-Z][a-zA-Z]*';
-    var regExPhone: string = '[2-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]';
-    var regExAge: string = '[1]*[0-9]?[0-9]';
-    var regExLettersOnly: string = '[a-ZA-Z]+';
-    var regExNumbersOnly: string = '[0-9]*';
-    var regExZip: string = '[0-9]{5}[-]?[0-9]?[0-9]?[0-9]?[0-9]?';
-
-    this.collaboratorForm = fb.group({  
-        'enterFullName': ['', Validators.required],
-        'enterContact': ['', Validators.compose([Validators.required, Validators.pattern(regExEmail)])],
-        'enterAreasOfExpertise': ['', Validators.required],
-        'enterDesiredContribution': ['', Validators.required],
-        'enterRelevantLinks': ['',Validators.required],
-        
-
-    });
-    
-// End Constructor
+ngOnInit() {
+  this.user = this.userSvc.getUser() || this.userSvc.getNewUser();
+  this.fullName = `${this.user.firstName} ${this.user.lastName}`
+  this.collaboratorForm = this.fb.group({  
+    'enterFullNameCtrl': [this.fullName, Validators.required],
+    'enterEmailAddressCtrl': [this.user.emailAddress, Validators.compose([Validators.required, Validators.pattern(this.regExEmail)])],
+    'enterAreasOfExpertiseCtrl': ['', Validators.required],
+    'enterContributionCtrl': ['', Validators.required],
+    'enterRelevantLinksCtrl': ['',Validators.required],
+  });
 }
 
 
