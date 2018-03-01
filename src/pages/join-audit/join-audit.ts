94c3ded306a1722d192d41af2f54a76fe54a9af0
Melissa Schwartz <melissjs@gmail.com>
2018-02-28 23:13:12 -0800

    join audit

--- src/pages/join-audit/join-audit.ts
+++ src/pages/join-audit/join-audit.ts
@@ -1,3 +1,4 @@
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuditServiceProvider } from '../../providers/audit-service/audit-service';
@@ -19,11 +20,21 @@ export class JoinAuditPage implements OnInit {
   lead: boolean;
   auditor: boolean;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private auditSvc: AuditServiceProvider) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private auditSvc: AuditServiceProvider, private alertCtrl: AlertController) {
     this.pageTitle = 'Join Audit';
+    this.auditor = true;
   }
 
   ngOnInit() {
+  }
+
+  onInfoAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'What are auditor and lead roles?',
+      subTitle: 'If you join an audit you are automatically an auditor. There are two lead roles positions per audit, you can apply to be a lead auditor if you want to be paid and take on more responsibility. You will be contacted if your request is approved.',
+      buttons: ['Dismiss']
+    });
+    alert.present();
   }
 
 }
