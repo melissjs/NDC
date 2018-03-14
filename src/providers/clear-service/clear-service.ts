e4670559f59e18b27469446118eb9b820724161d
Melissa Schwartz <melissjs@gmail.com>
2018-03-14 12:22:25 -0700

    clearSvc

--- src/providers/clear-service/clear-service.ts
+++ src/providers/clear-service/clear-service.ts
@@ -2,7 +2,7 @@ import { AuditServiceProvider } from './../audit-service/audit-service';
 import { UserServiceProvider } from './../user-service/user-service';
 import { ResumeRoleServiceProvider } from './../resume-role-service/resume-role-service';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, Inject, forwardRef } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class ClearServiceProvider {
