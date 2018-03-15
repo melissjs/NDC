f66f2c496a496908bf59279ff5b249e08d9ecaba
Melissa Schwartz <melissjs@gmail.com>
2018-03-14 18:26:58 -0700

    volunteer info

--- src/components/roles/roles.ts
+++ src/components/roles/roles.ts
@@ -1,5 +1,7 @@
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 import { UserServiceProvider } from './../../providers/user-service/user-service';
 import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'roles',
@@ -9,10 +11,21 @@ export class RolesComponent {
 
   roles: string[];
 
-  constructor(private userSvc: UserServiceProvider) {
+  constructor(private userSvc: UserServiceProvider, private alertCtrl: AlertController, private navCtrl: NavController) {
     this.roles = this.userSvc.getUser().userRoles;
   }
 
   addRole() {
+    this.navCtrl.push('RequestRolePage');
   }
+
+  onInfoAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Did I request a volunteer role?',
+      subTitle: 'Any role other than user implies volunteer status in our database. You are not assigned to any actual individual volunteering position from this role.',
+      buttons: ['Dismiss']
+    });
+    alert.present();
+  }
+
 }
